22b2f6c0d3c4abdc0799ac55ce5ff2f6f3f7f723
JBLOGNON <jean-baptiste.lognon@etu.unice.fr>
2021-10-30 13:51:06 +0200

    Update du filtre (bug ou la mise a jours et fait apres 2 click au lieu de 1)

--- lib/navigation/acheter.dart
+++ lib/navigation/acheter.dart
@@ -50,7 +50,6 @@ class __AcheterState extends State<Acheter> with TickerProviderStateMixin {
     FontAwesomeIcons.infinity,
     FontAwesomeIcons.hatCowboySide,
     FontAwesomeIcons.tshirt,
-    FontAwesomeIcons.tshirt,
     FontAwesomeIcons.socks,
     FontAwesomeIcons.ring,
   ];
@@ -164,11 +163,11 @@ class __AcheterState extends State<Acheter> with TickerProviderStateMixin {
             controller: _controller,
             children: <Widget>[
               _generateProduct(),
-              FaIcon(_icons[1]),
-              FaIcon(_icons[2]),
-              FaIcon(_icons[3]),
-              _generateProductShoes(),
-              FaIcon(_icons[5])
+              _generateProduct(),
+              _generateProduct(),
+              //_generateProductShoes(),
+              _generateProduct(),
+              _generateProduct()
             ],
           ),
         ),
@@ -180,6 +179,25 @@ class __AcheterState extends State<Acheter> with TickerProviderStateMixin {
     Stream<QuerySnapshot> _productsStream =
         FirebaseFirestore.instance.collection('Products').snapshots();
 
+    var productType = [
+      "Hat",
+      "Jacket",
+      "Tshirt",
+      "Pants",
+      "Shoes",
+      "Accessory"
+    ];
+
+    if (_controller.index == 1) {
+      productType = ["Hat"];
+    } else if (_controller.index == 2) {
+      productType = ["Jacket"];
+    } else if (_controller.index == 3) {
+      productType = ["Shoes"];
+    } else if (_controller.index == 4) {
+      productType = ["Accessory"];
+    }
+
     return StreamBuilder<QuerySnapshot>(
       stream: _productsStream,
       builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
@@ -188,195 +206,14 @@ class __AcheterState extends State<Acheter> with TickerProviderStateMixin {
         }
 
         if (snapshot.connectionState == ConnectionState.waiting) {
-          return Container(
-            child: Stack(
-              children: <Widget>[
-                Container(
-                  alignment: AlignmentDirectional.center,
-                  decoration: const BoxDecoration(
-                    color: Colors.white70,
-                  ),
-                  child: Container(
-                    decoration: BoxDecoration(
-                        color: Colors.red[100],
-                        borderRadius: BorderRadius.circular(10.0)),
-                    width: 300.0,
-                    height: 200.0,
-                    alignment: AlignmentDirectional.center,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        const Center(
-                          child: SizedBox(
-                            height: 50.0,
-                            width: 50.0,
-                            child: CircularProgressIndicator(
-                              value: null,
-                              strokeWidth: 7.0,
-                            ),
-                          ),
-                        ),
-                        Container(
-                          margin: const EdgeInsets.only(top: 25.0),
-                          child: const Center(
-                            child: Text(
-                              "Loading...",
-                              style: TextStyle(color: Colors.white),
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          );
+          return Text(" ");
         }
 
         return ListView(
-          children: snapshot.data!.docs.map((DocumentSnapshot document) {
-            Map<String, dynamic> data =
-                document.data()! as Map<String, dynamic>;
-            return Container(
-              margin: const EdgeInsets.only(
-                  top: 10.0, bottom: 10.0, left: 20.0, right: 20.0),
-              child: Card(
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(15.0),
-                    side: BorderSide(
-                      color: Colors.grey.withOpacity(0.3),
-                      width: 2,
-                    )),
-                child: InkWell(
-                  splashColor: Colors.red.withAlpha(30),
-                  onTap: () {},
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: <Widget>[
-                      Padding(
-                        padding: const EdgeInsets.only(left: 5.0),
-                        child: Image.network(
-                          data["productImages"][0],
-                          height: 100,
-                          width: 100,
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Column(
-                          mainAxisSize: MainAxisSize.min,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            Text(
-                              data["productBrand"],
-                              style: const TextStyle(
-                                  color: Colors.red, fontSize: 18),
-                              textAlign: TextAlign.left,
-                            ),
-                            Text(
-                                data["productName"] +
-                                    " (" +
-                                    data["productSize"].toString() +
-                                    ")",
-                                style: const TextStyle(
-                                    color: Colors.black, fontSize: 16),
-                                textAlign: TextAlign.left),
-                            Text(
-                              data["productPrice"].toString() + "€",
-                              style: const TextStyle(
-                                  color: Colors.black, fontSize: 16),
-                            ),
-                          ],
-                        ),
-                      ),
-                      const Expanded(child: SizedBox()),
-                      Padding(
-                        padding: const EdgeInsets.only(right: 15.0),
-                        child: IconButton(
-                          onPressed: () {},
-                          color: Colors.red,
-                          icon: const Icon(
-                            Icons.shopping_cart,
-                            size: 25,
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            );
-          }).toList(),
-        );
-      },
-    );
-  }
-
-  _generateProductShoes() {
-    Stream<QuerySnapshot> _productsStreamShoes = FirebaseFirestore.instance
-        .collection('Products')
-        .where("productType", isEqualTo: "Shoes")
-        .snapshots();
-
-    return StreamBuilder<QuerySnapshot>(
-      stream: _productsStreamShoes,
-      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-        if (snapshot.hasError) {
-          return const Text('Something went wrong');
-        }
-
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return Container(
-            child: Stack(
-              children: <Widget>[
-                Container(
-                  alignment: AlignmentDirectional.center,
-                  decoration: const BoxDecoration(
-                    color: Colors.white70,
-                  ),
-                  child: Container(
-                    decoration: BoxDecoration(
-                        color: Colors.red[100],
-                        borderRadius: BorderRadius.circular(10.0)),
-                    width: 300.0,
-                    height: 200.0,
-                    alignment: AlignmentDirectional.center,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        const Center(
-                          child: SizedBox(
-                            height: 50.0,
-                            width: 50.0,
-                            child: CircularProgressIndicator(
-                              value: null,
-                              strokeWidth: 7.0,
-                            ),
-                          ),
-                        ),
-                        Container(
-                          margin: const EdgeInsets.only(top: 25.0),
-                          child: const Center(
-                            child: Text(
-                              "Loading...",
-                              style: TextStyle(color: Colors.white),
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          );
-        }
-
-        return ListView(
-          children: snapshot.data!.docs.map((DocumentSnapshot document) {
+          children: snapshot.data!.docs
+              .where((DocumentSnapshot documentSnapshot) =>
+                  productType.contains(documentSnapshot["productType"]))
+              .map((DocumentSnapshot document) {
             Map<String, dynamic> data =
                 document.data()! as Map<String, dynamic>;
             return Container(
